06139c5fb61a6af438c134d8c82f741dc6f5f417
Dinesh983 <rdinesh05306@gmail.com>
2021-04-03 12:36:17 +0530

    Adding Response middleware

--- src/app/todos/routes/public.routes.js
+++ src/app/todos/routes/public.routes.js
@@ -7,6 +7,7 @@ const router = Router();
 
 router.get('/', TodoController.getAll);
 router.get('/:id', TodoController.getOne);
+router.get('/:id/disable', TodoController.getOne);
 router.post('/', TodoController.create);
 router.put('/:id', TodoController.update);
 router.delete('/:id', TodoController.deleteOne);
